ca03141ea41ab73db35199596c8281e24b118c76
Lee Sutton <leesutton1@gmail.com>
2019-11-11 10:44:38 -0800

    fixed user controller tests

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
-    @user = users(:one)
+    @user = users(:lee)
   end
 
   test "should get index" do
@@ -17,7 +17,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
 
   test "should create user" do
     assert_difference('User.count') do
-      post users_url, params: { user: { email: @user.email, password_digest: @user.password_digest, username: @user.username } }
+      post users_url, params:
+          {user: {email: @user.email, password: "password",
+           password_confirmation: "password",
+           username: @user.username}}
     end
 
     assert_redirected_to '/'
@@ -34,7 +37,11 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update user" do
-    patch user_url(@user), params: { user: { email: @user.email, password_digest: @user.password_digest, username: @user.username } }
+    patch user_url(@user), params:
+        {user: {email: @user.email, password: "password",
+         password_confirmation: "password",
+         username: @user.username}}
+
     assert_redirected_to user_url(@user)
   end
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
+  def setup
+    @user = User.new(username: "lmsutton", password: "password",
+                     password_confirmation: "password",
+                     email: "leesutton1@gmail.com")
+  end
+
+  # test "password should be present" do
+  #   @user.password = @user.password_confirmation = " " * 6
+  #   assert_not @user.valid?
   # end
 end
