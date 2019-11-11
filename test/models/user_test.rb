7bab2b1ebb203bba52b51ca7c3b831a6ff451061
Lee Sutton <leesutton1@gmail.com>
2019-11-11 11:50:26 -0800

    added valid email tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,10 +12,31 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "email addresses should be valid" do
+    @user.email = "dummy@email,com"
+    assert_not @user.valid?
+  end
+
+  test "email addresses should contain a name" do
+    @user.email = "@email.com"
+    assert_not @user.valid?
+  end
+
+  test "email addresses should contain a host" do
+    @user.email = "test@.com"
+    assert_not @user.valid?
+  end
+
+  test "email addresses should contain a domain" do
+    @user.email = "test@email."
+    assert_not @user.valid?
+  end
+
   test "email addresses should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
+
 end
